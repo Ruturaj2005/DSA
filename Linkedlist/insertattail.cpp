da9b149676529bd5a1315927086e17e1e3a594fe
Ruturaj2005 <153944260+Ruturaj2005@users.noreply.github.com>
2024-06-11 12:09:44 +0530

    My linkedlist codes

---
+++ Linkedlist/insertattail.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+    
+    Node(int d){
+        this->data=d;
+        this->next=NULL;
+    }
+    
+};
+void insertattail(Node* &tail,int d){
+        Node* temp=new Node(d);
+        tail->next=temp;
+        tail=temp;
+    }
+void print(Node* &head){
+    Node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+}
+int main(){
+    Node* Node1= new Node(10);
+    cout<<Node1->data<<endl;
+    cout<<Node1->next<<endl;
+    Node* tail=Node1;
+    Node* head=Node1;
+    insertattail(tail,12);
+    print(head);
+
+}
